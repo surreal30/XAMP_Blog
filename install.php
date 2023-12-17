ac1e9f66e7f9602f33273a11be97aa22f254d912
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-18 01:57:07 +0530

    feat: add session to install.php

--- install.php
+++ install.php
@@ -2,65 +2,99 @@
 
 require_once "lib/common.php";
 
-$root = getRootPath();
-$database = getDatabasePath();
-
-$error = '';
-
-// A security measure, to avoid anyone resetting the database if it already exists
-if (is_readable($database) && filesize($database) > 0)
+function installBlog()
 {
-    $error = 'Please delete the existing database manually before installing it afresh';
-}
 
-// Create an empty file for the database
-if (!$error)
-{
-    $createdOk = @touch($database);
-    if (!$createdOk)
+
+    $root = getRootPath();
+    $database = getDatabasePath();
+
+    $error = '';
+
+    // A security measure, to avoid anyone resetting the database if it already exists
+    if (is_readable($database) && filesize($database) > 0)
     {
-        $error = sprintf(
-            'Could not create the database, please allow the server to create new files in \'%s\'',
-            dirname($database)
-        );
+        $error = 'Please delete the existing database manually before installing it afresh';
     }
-}
 
-// Grab the SQL commands we want to run on the database
-if (!$error)
-{
-    $sql = file_get_contents($root . '/data/init.sql');
-    if ($sql === false)
+    // Create an empty file for the database
+    if (!$error)
     {
-        $error = 'Cannot find SQL file';
-    }
-}
-
-// Connect to the new database and try to run the SQL commands
-if (!$error)
-{
-    $pdo = getPDO();
-    $result = $pdo->exec($sql);
-    if ($result === false)
-    {
-        $error = 'Could not run SQL: ' . print_r($pdo->errorInfo(), true);
-    }
-}
-
-// See how many rows we created, if any
-$count = [];
-
-foreach (['post', 'comment'] as $tableName)
-{
-    if(!$error)
-    {
-        $sql = "SELECT COUNT(*) AS c FROM " . $tableName;
-        $stmt = $pdo->query($sql);
-        if ($stmt)
+        $createdOk = @touch($database);
+        if (!$createdOk)
         {
-            $count[$tableName] = $stmt->fetchColumn();
+            $error = sprintf(
+                'Could not create the database, please allow the server to create new files in \'%s\'',
+                dirname($database)
+            );
         }
     }
+
+    // Grab the SQL commands we want to run on the database
+    if (!$error)
+    {
+        $sql = file_get_contents($root . '/data/init.sql');
+        if ($sql === false)
+        {
+            $error = 'Cannot find SQL file';
+        }
+    }
+
+    // Connect to the new database and try to run the SQL commands
+    if (!$error)
+    {
+        $pdo = getPDO();
+        $result = $pdo->exec($sql);
+        if ($result === false)
+        {
+            $error = 'Could not run SQL: ' . print_r($pdo->errorInfo(), true);
+        }
+    }
+
+    // See how many rows we created, if any
+    $count = [];
+
+    foreach (['post', 'comment'] as $tableName)
+    {
+        if(!$error)
+        {
+            $sql = "SELECT COUNT(*) AS c FROM " . $tableName;
+            $stmt = $pdo->query($sql);
+            if ($stmt)
+            {
+                $count[$tableName] = $stmt->fetchColumn();
+            }
+        }
+    }
+
+    return [$count, $error];
+}
+
+// Create session
+session_start();
+
+// Only run the installer when responding to form
+if($_POST)
+{
+    list($_SESSION['count'], $_SESSION['error']) = installBlog();
+
+    $host = $_SERVER['HTTP_HOST'];
+    $script = $_SERVER['REQUEST_URI'];
+    header("Location: http://" . $host . $script);
+    exit();
+}
+
+$attempted = false;
+
+if($_SESSION)
+{
+    $attempted = true;
+
+    $count = $_SESSION['count'];
+    $error = $_SESSION['error'];
+
+    // Unset session variable to report failure only once
+    unset($_SESSION['count'], $_SESSION['error']);
 }
 
 ?>
@@ -84,22 +118,33 @@ foreach (['post', 'comment'] as $tableName)
         </style>
     </head>
     <body>
-        <?php if ($error): ?>
-            <div class="error box">
-                <?php echo $error ?>
-            </div>
-        <?php else: ?>
-            <div class="success box">
-                The database and demo data was created OK. <br>
-                <?php foreach (['post', 'comment'] as $tableName)
-                {
-                    if(isset($count[$tableName]))
+        <?php if($attempted): ?>
+            <?php if ($error): ?>
+                <div class="error box">
+                    <?php echo $error ?>
+                </div>
+            <?php else: ?>
+                <div class="success box">
+                    The database and demo data was created OK. <br>
+                    <?php foreach (['post', 'comment'] as $tableName)
                     {
-                        echo $count[$tableName] . " new " . $tableName . " were created! <br>";
+                        if(isset($count[$tableName]))
+                        {
+                            echo $count[$tableName] . " new " . $tableName . " were created! <br>";
+                        }
                     }
-                }
-                ?>
-            </div>
-        <?php endif ?>
+                    ?>
+                </div>
+                <p>
+                    <a href="index.php">View the blog</a>, or <a href="install.php">install again</a>
+                </p>
+            <?php endif; ?>
+        <?php else: ?>
+            <p> Click the install button to reset the database. </p>
+
+            <form method="post">
+                <input name="install" type="submit" value="Install">
+            </form>
+        <?php endif; ?>
     </body>
 </html>
