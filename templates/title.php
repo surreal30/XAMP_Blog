0655f7c942beee4056a3b50c78dacdf37cefc3fc
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-22 02:34:52 +0530

    feat: call isLoggedIn() and display login or logout option accordingly

--- templates/title.php
+++ templates/title.php
@@ -1,5 +1,9 @@
 <div style="float: right">
-        <a href="login.php"> Login </a>
+        <?php if(isLoggedIn()): ?>
+        <a href="logout.php"> Logout </a>
+        <?php else: ?>
+                <a href="login.php"> Login </a>
+        <?php endif; ?>
 </div>
 <a href="index.php">
         <h1>Blog title</h1>
