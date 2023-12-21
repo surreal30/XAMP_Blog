423bfa80ba5d29eac93d46f7c461d178be52df8d
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-21 20:33:41 +0530

    feat: create table user, add and display user/password

--- data/init.sql
+++ data/init.sql
@@ -95,3 +95,11 @@ INSERT INTO
         'Appreciate it!'
     )
 ;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    created_at VARCHAR NOT NULL,
+    is_enabled BOOLEAN NOT NULL DEFAULT true
+);
--- install.php
+++ install.php
@@ -10,22 +10,37 @@ session_start();
 if($_POST)
 {
     $pdo = getPDO();
-    list($_SESSION['count'], $_SESSION['error']) = installBlog($pdo);
+    list($rowCount, $error) = installBlog($pdo);
+
+    $password = '';
+    if(!$error)
+    {
+        $username = "admin";
+        list($password, $error) = createUser($pdo, $username);
+    }
+
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+    $_SESSION['count'] = $rowCount;
+    $_SESSION['error'] = $error;
+    $_SESSION['try-install'] = true;
 
     redirectAndExit('install.php');
 }
 
 $attempted = false;
 
-if($_SESSION)
+if(isset($_SESSION['try-install']))
 {
     $attempted = true;
 
     $count = $_SESSION['count'];
     $error = $_SESSION['error'];
+    $username = $_SESSION['username'];
+    $password = $_SESSION['password'];
 
     // Unset session variable to report failure only once
-    unset($_SESSION['count'], $_SESSION['error']);
+    unset($_SESSION['count'], $_SESSION['error'], $_SESSION['username'], $_SESSION['password'], $_SESSION['try-install']);
 }
 
 ?>
@@ -57,6 +72,8 @@ if($_SESSION)
             <?php else: ?>
                 <div class="success box">
                     The database and demo data was created OK. <br>
+
+                    <?php // Report the count for each table ?>
                     <?php foreach (['post', 'comment'] as $tableName)
                     {
                         if(isset($count[$tableName]))
@@ -65,6 +82,12 @@ if($_SESSION)
                         }
                     }
                     ?>
+
+                    <?php // Report new password ?>
+                    The new '<?php echo htmlEscape($username); ?>' password is
+                    <span style="font-size: 1.2 em;">
+                        <?php echo htmlEscape($password); ?>
+                    </span> (You can copy it if you want.)
                 </div>
                 <p>
                     <a href="index.php">View the blog</a>, or <a href="install.php">install again</a>
