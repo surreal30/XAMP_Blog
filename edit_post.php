2fc820829da552b0933c7e5496ed1ac21cf49f6c
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-23 03:49:27 +0530

    feat: create edit_post to create new post

---
+++ edit_post.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'lib/common.php';
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>A Blog Implementation | New Post</title>
+	<?php require_once "templates/head.php"; ?>
+</head>
+<body>
+	<?php require_once "templates/title.php"; ?>
+
+	<form method="post" class="post-form user-form">
+		<div>
+			<label for="post-title">Title:</label>
+			<input type="text" name="post-title" id="post-title">
+		</div>
+		<div>
+			<label for="post-body">Body:</label>
+			<textarea id="post-body" name="post-body" rows="12" cols="70"></textarea>
+		</div>
+		<div>
+			<input type="submit" name="submit" value="Save post">
+		</div>
+	</form>
+
+</body>
+</html>
--- templates/title.php
+++ templates/title.php
@@ -1,6 +1,7 @@
 <div class="top-menu">
         <div class="menu-options">
                 <?php if(isLoggedIn()): ?>
+                        <a href="edit_post.php"> New Post </a>
                         Hello, <?php echo htmlEscape(getAuthUser()); ?>
                 <a href="logout.php"> Logout </a>
                 <?php else: ?>
