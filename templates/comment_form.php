90c9891f541ae234f1c0c333eeb13dac85b19eae
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-22 17:00:36 +0530

    refactor: use css class to improve comment form ui

--- templates/comment_form.php
+++ templates/comment_form.php
@@ -6,7 +6,7 @@
 ?>
 
 <?php if($errors): ?>
-	<div class="error box">
+	<div class="error box comment-margin">
 		<ul>
 			<?php foreach($errors as $error): ?>
 				<li><?php echo $error; ?></li>
@@ -17,24 +17,26 @@
 
 <h3>Add your comments</h3>
 
-<form method="post">
-	<p>
+<form method="post" class="comment-form">
+	<div>
 		<label for="comment-name">
 			Name:
 		</label>
 		<input type="text" name="comment-name" id="comment-name" value="<?php echo htmlEscape($commentData['name']); ?>">
-	</p>
-	<p>
+	</div>
+	<div>
 		<label for="comment-website">
 			Website:
 		</label>
 		<input type="text" name="comment-website" id="comment-website" value="<?php echo htmlEscape($commentData['website']); ?>">
-	</p>
-	<p>
+	</div>
+	<div>
 		<label for="comment-text">
 			Comment:
 		</label>
 		<textarea type="text" name="comment-text" id="comment-text" rows="8" cols="70" value="<?php echo htmlEscape($commentData['text']); ?>"></textarea>
-	</p>
-	<input type="submit" value="Submit comment" />
+	</div>
+	<div>
+		<input type="submit" value="Submit comment" />
+	</div>
 </form>
