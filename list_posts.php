3e03b3b3cbdfc2f952e7c87e0f3e6b78b03c7bb0
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-24 03:47:01 +0530

    feat: create list_posts page

---
+++ list_posts.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once "lib/common.php";
+
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>A Blog Application | Blog Posts</title>
+	<?php require_once "templates/head.php"; ?>
+</head>
+<body>
+	<?php require_once "templates/top_menu.php"; ?>
+
+	<h1>Post List</h1>
+
+	<form method="post">
+		<table id="post-list">
+			<tbody>
+				<tr>
+					<td>
+						Title of the first post
+					</td>
+					<td>
+						<a href="edit_post.php?post_id=1">Edit</a>
+					</td>
+					<td>
+						<input type="submit" name="post[1]" value="delete">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Title of the second post
+					</td>
+					<td>
+						<a href="edit_post.php?post_id=2">Edit</a>
+					</td>
+					<td>
+						<input type="submit" name="post[2]" value="delete">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Title of the third post
+					</td>
+					<td>
+						<a href="edit_post.php?post_id=3">Edit</a>
+					</td>
+					<td>
+						<input type="submit" name="post[3]" value="delete">
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+
+</body>
+</html>
