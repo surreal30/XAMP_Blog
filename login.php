ff5801b1f766cd30f500abe1053993689887a54f
surreal <2002vaibhavlakhera3@gmail.com>
2023-12-25 19:04:04 +0530

    feat: add label for login form

--- login.php
+++ login.php
@@ -58,14 +58,18 @@ if($_POST)
 
 	<p>Login here</p>
 	<form method="post">
-		<p>
-			Username: 
-			<input type="text" name="username" value="<?php echo htmlEscape($username); ?>">
-		</p>
-		<p>
-			Password: 
-			<input type="text" name="password">
-		</p>
+		<div>
+			<label for="username">
+				Username: 
+			</label>
+			<input type="text" name="username" id="username" value="<?php echo htmlEscape($username); ?>">
+		</div>
+		<div>
+			<label for="password">
+				Password: 
+			</label>
+			<input type="text" id="password" name="password">
+		</div>
 		<input type="submit" name="submit" value="Login">
 	</form>
 
